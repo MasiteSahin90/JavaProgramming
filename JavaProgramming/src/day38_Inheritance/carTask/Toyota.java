9df800e92e4b20327267abb84a91a90e5948ffd8
masitesahin90 <masitesahin90@gmail.com>
2022-07-29 22:12:02 +0100

    i have just started to use git

---
+++ JavaProgramming/src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,18 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends Car {
+
+
+    public Toyota( String model, int year, double price, String color, int miles) {
+        super("Totota", model, year, price, color, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand + " "+ model + " is reliable");
+    }
+
+
+
+
+
+}
