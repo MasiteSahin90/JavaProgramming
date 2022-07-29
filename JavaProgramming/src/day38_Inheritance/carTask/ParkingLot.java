f4699f78a36fbff67b450e786bdd8cfcf224d4a5
masitesahin90 <masitesahin90@gmail.com>
2022-07-29 22:25:39 +0100

    day38 inheritNCE ParkingLot

---
+++ JavaProgramming/src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,17 @@
+package day38_Inheritance.carTask;
+
+import day32_Constructor.Test;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota t = new Toyota("camry",123,2345.4,"fddd",2345);
+
+        Tesla tes = new Tesla("erere",12,2345.6,"rewr",2345);
+
+        BMW b = new BMW("dfd",123,234,"asdae",23456);
+
+
+    }
+}
