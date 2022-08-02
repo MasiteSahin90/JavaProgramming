fc809fe04c30f7f81d4727230800a5ace9bb7224
masitesahin90 <masitesahin90@gmail.com>
2022-08-02 23:32:07 +0100

    day39 Shapes

--- JavaProgramming/src/day38_Inheritance/shapes/ShapesObject.java
+++ JavaProgramming/src/day38_Inheritance/shapes/ShapesObject.java
@@ -1,4 +1,32 @@
 package day38_Inheritance.shapes;
 
+import java.util.concurrent.Callable;
+
 public class ShapesObject {
+
+    public static void main(String[] args) {
+
+        Square s = new Square(7);
+
+        Circle c = new Circle(3);
+
+        Rectangle r = new Rectangle(3,4);
+
+        System.out.println(s);
+        System.out.println(c);
+        System.out.println(r);
+
+        s.setSide(5);
+
+        System.out.println(s);
+
+        r.setWidth(3);
+        r.setLength(2);
+
+        System.out.println(r);
+
+
+    }
+
+
 }
