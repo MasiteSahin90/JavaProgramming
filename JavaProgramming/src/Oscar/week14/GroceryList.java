6d929c58329d342d10dfbbb9900b89016c974e20
masitesahin90 <masitesahin90@gmail.com>
2022-08-07 19:17:38 +0100

    oscar review

--- JavaProgramming/src/Oscar/week14/GroceryList.java
+++ JavaProgramming/src/Oscar/week14/GroceryList.java
@@ -1,4 +1,39 @@
 package Oscar.week14;
 
+import java.util.ArrayList;
+
 public class GroceryList {
+
+    private ArrayList<String> groceryList;
+
+
+    public GroceryList() {
+        this.groceryList = new ArrayList<>();
+    }
+
+    public void addAnItem(String item){
+        groceryList.add(item);
+    }
+
+    public void removeAnItem(int position){
+        groceryList.remove(position-1);
+    }
+
+    public void searchAnItem(String item){
+        groceryList.add(item);
+    }
+
+    public void modifyAnItem(int position , String newItem){
+        groceryList.set((position-1),newItem );
+        System.out.println("Grocery item " + (position) + " has been modified");
+    }
+
+    @Override
+    public String toString() {
+        String list = "You have "+ groceryList.size() + " items in your shopping list";
+        for (int i=0; i< groceryList.size();i++){
+            list+="\n" +(i+1) + "." + groceryList.get(i);
+        }
+        return list;
+    }
 }
--- JavaProgramming/src/Oscar/week14/ShopListMakerApp.java
+++ JavaProgramming/src/Oscar/week14/ShopListMakerApp.java
@@ -1,14 +1,18 @@
 package Oscar.week14;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ShopListMakerApp {
+
+
 
     public static Scanner scanner; // global variable for this class
 
     static int x; //we can use x in everywhere because it is a global variable
 
     public static void main(String[] args) {
+        GroceryList list = new GroceryList();
 
         printInstructions();
 
@@ -20,6 +24,24 @@ public class ShopListMakerApp {
             System.out.println("Enter your next choice: ");
             int choice = scanner.nextInt();
             switch (choice){
+                case 1:
+                    System.out.println(list);
+                case 2:
+                    System.out.println("Enter your grocery item");
+                    list.addAnItem(scanner.nextLine());
+                    scanner.nextLine();
+                    break;
+                case 3:
+                    System.out.println("Enter item number to be modified : ");
+                    int itemNo = scanner.nextInt();
+                    String newItem = scanner.nextLine();
+                    list.modifyAnItem(itemNo,newItem);
+                    break;
+                case 4:
+                    System.out.println("Enter the item number that will be removed");
+                    int itemNumber = scanner.nextInt();
+                    list.removeAnItem(itemNumber);
+                    break;
                 case 6:
                     toContinue = false;
                     break;
